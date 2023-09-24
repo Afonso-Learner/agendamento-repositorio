4918027ab63cdc0f9357fc4fe56721a3a09d6120
Simão Afonso <afonso.learner@gmail.com>
2023-09-24 21:33:44 +0100

    commit

---
+++ app/Config/Routes.php
@@ -0,0 +1,12 @@
+<?php
+namespace Config;
+use App\Controllers\Super\HomeController;
+use CodeIgniter\Router\RouteCollection;
+
+/**
+ * @var RouteCollection $routes
+ */
+$routes->get('/', 'Home::index');
+
+$routes->get('super', [HomeController::class, 'index'], ['as' => 'super.home']);
+
